805c497ceb8de210c06b7d17eed857aa6baa40d8
Jesse Shiao <30820084+jesse850330@users.noreply.github.com>
2020-02-18 14:50:45 +1300

    First Update

---
+++ A jelly Good Time/Assets/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static int brickCount;
+
+    public static void ReloadThisScene() {
+        Scene current = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(current.name);
+    }
+
+    public static bool LevelClear {
+        get {
+
+            if (brickCount<=0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+
+    void Start()
+    {
+
+        brickCount = GameObject.FindGameObjectsWithTag(tags.block.ToString()).Length;
+        Debug.Log("Have "+brickCount+" Breakable Blocks");
+    }
+
+    void Update()
+    {
+
+    }
+}
+
+enum tags
+{
+    block, background, player, ball
+}

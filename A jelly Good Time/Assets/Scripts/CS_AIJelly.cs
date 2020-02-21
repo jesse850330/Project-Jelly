a49c57b764f5bcdfb45105ce767100301d5f1b99
Jesse Shiao <30820084+jesse850330@users.noreply.github.com>
2020-02-21 15:18:57 +1300

    FPP Control fixed

---
+++ A jelly Good Time/Assets/Scripts/CS_AIJelly.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_AIJelly : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    public float speed = 5;
+    public int step = 1;
+    public int x = 0;
+    public int z = 0;
+    public float velocity = 0.5f;
+    private Rigidbody rd;
+    private Vector3 NextPosition;
+    void Start()
+    {
+        NextPosition = transform.position;
+		InvokeRepeating("EnemyMove", 1, velocity);
+    }
+	void FixedUpdate()
+    {
+        rd.MovePosition(Vector3.Lerp(transform.position, NextPosition, Time.deltaTime * speed));
+    }
+
+    public void EnemyMove()
+    {
+
+        Vector3 offset = PlayerTransform.position - transform.position;
+
+        if (offset.magnitude < 12)
+        { 
+
+        }
+
+        else
+        { 
+            if (Mathf.Abs(offset.z) > Mathf.Abs(offset.x))
+            {
+
+                if (offset.z > 0)
+                { 
+                   x = 0;
+                   z = step;
+                }
+                else
+                {
+                   x = 0;
+                   z = -step;
+                   
+                }
+            }
+            else
+            {
+                if (offset.x > 0)
+                {
+                   x = step;
+                   z = 0;
+                   
+                }
+                else
+                {
+                   x = -step;
+                   z = 0;
+                }
+            }
+            NextPosition = transform.position + new Vector3(x, 0, z);
+        }
+    }
+
+}

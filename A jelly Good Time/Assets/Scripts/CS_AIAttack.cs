8519af0d56af5f547a125b93bf850c81ad9510f2
Jesse Shiao <30820084+jesse850330@users.noreply.github.com>
2020-02-25 15:26:21 +1300

    nav mesh

---
+++ A jelly Good Time/Assets/Scripts/CS_AIAttack.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+namespace ProjectJelly.FPP
+{
+    public class CS_AIAttack : MonoBehaviour
+    {
+        public const int maxHealth = 100;
+        public int currentHealth = maxHealth;
+        public void TakeDamage(int amount)
+        {
+            currentHealth -= amount;
+            if (currentHealth <= 0)
+            {
+                currentHealth = 0;
+                Destroy(gameObject);
+            }
+        }
+        void OnTriggerEnter(Collider basic)
+        {
+            currentHealth -= 5;
+        }
+
+    }
+}

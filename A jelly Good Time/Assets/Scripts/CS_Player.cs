0cd883778a7944aa94eee67aa74ab7faf0f2aaa5
Jesse Shiao <30820084+jesse850330@users.noreply.github.com>
2020-03-09 15:26:08 +1300

    Mag System

--- A jelly Good Time/Assets/Scripts/CS_Player.cs
+++ A jelly Good Time/Assets/Scripts/CS_Player.cs
@@ -6,16 +6,50 @@ namespace ProjectJelly.FPP
 {
     public class CS_Player : MonoBehaviour
     {
-
         public float BulletInt;
         public float MalletInt;
-        public AudioClip Fire;
-        public AudioClip Ability;
+        public int shellCount = 50;
+        public int currentCount;
+        private float currentFireTime;
+        public float loadTime = 1.5f;//Reload
+        private float currentLoadTime = 0;
+        public int chargerCount = 5;//Mags
+        // public AudioClip Fire;
+        // public AudioClip Ability;
         public GameObject Bullet;
         public GameObject Bullet_Position;
         public GameObject Mallet;
         public GameObject Mallet_Position;
         CS_FPPAttack AIHp;
+
+        void Update()
+        {
+
+            if (!isLoading()&& Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                StartCoroutine(FireWeapon());
+            }
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                StopAllCoroutines();
+            }
+            else if (currentCount == shellCount)
+            {
+                StopAllCoroutines();
+            }
+            if (Input.GetKey(KeyCode.R))
+            {
+                reLoading();
+            }
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                StartCoroutine(MalletAbility());
+            }
+            if (Input.GetKeyUp(KeyCode.Mouse1))
+            {
+                StopAllCoroutines();
+            }
+        }
 
         private void OnTriggerEnter(Collider other)
         {
@@ -24,15 +58,38 @@ namespace ProjectJelly.FPP
                 Destroy(gameObject);
             }
         }
+        public bool isLoading()
+        {
+            if (chargerCount == 0) return true;
+            if (currentCount < shellCount)
+            {
+                return false;
+            }
+            currentLoadTime += Time.deltaTime;
+            if (currentLoadTime >= loadTime)
+            {
+                currentLoadTime = 0;
+                return false;
+            }
+            return true;
+        }
 
-
+        public void reLoading()
+        {
+            if (currentCount >= 50)
+            {
+                chargerCount--;
+                currentCount = 0;
+            }
+        }
 
         IEnumerator FireWeapon()
         {
             while (true)
             {
                 Instantiate(Bullet, Bullet_Position.transform.position, Bullet_Position.transform.rotation);
-                AudioSource.PlayClipAtPoint(Fire, transform.localPosition);
+                currentCount++;
+                // AudioSource.PlayClipAtPoint(Fire, transform.localPosition);
                 yield return new WaitForSeconds(BulletInt);
             }
 
@@ -46,27 +103,9 @@ namespace ProjectJelly.FPP
                 pos.y = 2;
                 transform.position = pos;
                 Instantiate(Mallet, Mallet_Position.transform.position = pos, Mallet_Position.transform.rotation);
-                AudioSource.PlayClipAtPoint(Ability, transform.localPosition);
+                // AudioSource.PlayClipAtPoint(Ability, transform.localPosition);
                 yield return new WaitForSeconds(MalletInt);
             }
         }
-
-        void Update()
-        {
-
-            if (Input.GetKeyDown(KeyCode.Mouse0))
-            {
-                StartCoroutine(FireWeapon());
-            }
-            else if (Input.GetKeyUp(KeyCode.Mouse0))
-            {
-                StopAllCoroutines();
-            }
-            if (Input.GetKeyDown(KeyCode.Mouse1))
-            {
-            StartCoroutine(MalletAbility());
-            }
-        }
-
     }
 }

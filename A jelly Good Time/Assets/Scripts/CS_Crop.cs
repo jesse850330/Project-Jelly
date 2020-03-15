2ca05c656beaba292aa8ab9cb01a035c41093a33
Jesse Shiao <30820084+jesse850330@users.noreply.github.com>
2020-03-16 00:04:26 +1300

    crop HP

---
+++ A jelly Good Time/Assets/Scripts/CS_Crop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectJelly.FPP
+{
+    public class CS_Crop : MonoBehaviour
+    {
+        public float CropHp;
+
+
+        private void OnTriggerEnter(Collider theCollision)
+        {
+            if (theCollision.tag == "Enemy")
+            {
+                CropHp -= 1;
+            }
+        }
+        private void Update()
+        {
+            if (CropHp <= 0)
+            {
+                Destroy(gameObject, 1f);
+            }
+
+        }
+    }
+}
